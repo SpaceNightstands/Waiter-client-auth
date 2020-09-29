58c6fdda2911e5fdf7620890088389c7b9429d1c
Systool <systool@magisystem.xyz>
2020-09-29 23:35:29 +0200

    Added dotenv macro dependency

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,14 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern {
-    fn alert(s: &str);
+	fn alert(s: &str);
 }
 
+//Should scramble with const fn
+const JWT_SECRET: &str = dotenv_codegen::dotenv!("JWT_SECRET");
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, waiter-client-auth!");
+pub fn build_jwt() {
+	utils::set_panic_hook();
+	alert(JWT_SECRET);
 }
