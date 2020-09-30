b1c1b8edd0d38c46fc0718517ea568027738f68d
Systool <systool@magisystem.xyz>
2020-09-30 15:06:33 +0200

    Basic build_jwt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use js_sys::Object;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
+// Use `wee_alloc` as the global allocator.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
@@ -17,7 +14,45 @@ extern {
 const JWT_SECRET: &str = dotenv_codegen::dotenv!("JWT_SECRET");
 
 #[wasm_bindgen]
-pub fn build_jwt() {
-	utils::set_panic_hook();
-	alert(JWT_SECRET);
+pub fn build_jwt(val: &Object) -> String {
+	use hmac::NewMac;
+	use std::collections::HashMap;
+	use jwt::token::signed::SignWithKey;
+
+	let key = hmac::Hmac::<sha2::Sha256>::new_varkey(JWT_SECRET.as_bytes())
+    .unwrap();
+
+	use serde_json::Value;
+	let map: HashMap<String, Value> = Object::entries(val).iter()
+    .filter_map(
+			|kvpair| {
+				use wasm_bindgen::JsCast;
+				let kvpair = kvpair.dyn_ref::<js_sys::Array>()?;
+				if kvpair.length() == 2 {
+					/*let value = kvpair.get(1);
+					Some((
+						kvpair.get(0).as_string()?,
+						value.as_string()
+							.or_else(
+								|| value.as_f64().map(
+										|n| n.to_string()
+									 )
+							).or_else(
+								|| value.as_bool().map(
+										|n| n.to_string()
+									 )
+							).or_else(
+								|| value.dyn_ref::<Object>().map(
+										|obj| format!("{:?}", obj.to_string())
+									 )
+							)?
+					))*/
+					Some(kvpair.into_serde::<(String, Value)>().unwrap())
+				} else {
+					None
+				}
+			}
+		).collect();
+	map.sign_with_key(&key).unwrap()
 }
+
--- tests/web.rs
+++ tests/web.rs
@@ -4,13 +4,25 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
-
 use waiter_client_auth::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-	assert_eq!(1 + 1, 2);
-	greet();
+fn object_1() {
+	console_error_panic_hook::set_once();
+	use wasm_bindgen::{JsCast, JsValue};
+	let test = js_sys::Function::new_no_args("return {test: \"ABC\"}").call0(&JsValue::NULL).unwrap()
+		.dyn_into::<js_sys::Object>()
+		.unwrap();
+	assert_eq!(build_jwt(&test), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoiQUJDIn0.i1J1hhBpHZT70_gZZipdE_cWsZJxAoK3p03I7FjcfsA");
+}
+
+fn object_2() {
+	console_error_panic_hook::set_once();
+	use wasm_bindgen::{JsCast, JsValue};
+	let test = js_sys::Function::new_no_args("return {test: 123}").call0(&JsValue::NULL).unwrap()
+		.dyn_into::<js_sys::Object>()
+		.unwrap();
+	assert_eq!(build_jwt(&test), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoxMjN9.yFfpIQpxtxzaXgLENVd9yDOfq9rA9l17E2aPLqmRmiA");
 }
