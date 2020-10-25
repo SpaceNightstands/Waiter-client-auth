53b40f7f16edb15ad2fb3d3e32e865df65e81865
Systool <systool@magisystem.xyz>
2020-10-26 00:06:22 +0100

    Added scrambling macro

---
+++ waiter-client-auth-macros/src/lib.rs
@@ -0,0 +1,40 @@
+use proc_macro::TokenStream as TS;
+use proc_macro2::TokenStream;
+use quote::quote;
+use rand::Rng;
+
+#[proc_macro]
+pub fn scramble(input: TS) -> TS {
+	scramble_impl(input.into()).into()
+}
+
+fn scramble_impl(input: TokenStream) -> TokenStream {
+	let name = syn::parse2::<syn::LitStr>(input)
+    .unwrap()
+    .value();
+	let secret = dotenv::var(name).unwrap();
+	let secret = secret.as_bytes();
+	let mask = rand::thread_rng().sample_iter(rand::distributions::Standard)
+    .take(secret.len())
+    .collect::<Vec<u8>>();
+	let complementary = secret.iter()
+    .zip(mask.iter())
+    .map(
+			|(src, mask): (&u8, &u8)| mask ^ src
+		).collect::<Vec<u8>>();
+
+	let length = secret.len();
+	//let ident = syn::Ident::new(name);
+	quote!(
+		const JWT_SECRET: [(u8, u8); #length] = [#((#mask, #complementary)),*];
+	).into()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn scramble_impl() {
+			let array = super::scramble_impl(quote::quote!("JWT_SECRET"));
+			println!("{}", array);
+    }
+}
