28926d97c0ed5ee283ddbe92b8d20c2e21ecf250
Systool <systool@magisystem.xyz>
2020-10-26 18:28:36 +0100

    Macro test

--- tests/web.rs
+++ tests/web.rs
@@ -5,25 +5,35 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 use waiter_client_auth::*;
+use wasm_bindgen::{JsCast, JsValue};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn object_1() {
 	console_error_panic_hook::set_once();
-	use wasm_bindgen::{JsCast, JsValue};
 	let test = js_sys::Function::new_no_args("return {test: \"ABC\"}").call0(&JsValue::NULL).unwrap()
 		.dyn_into::<js_sys::Object>()
 		.unwrap();
-	assert_eq!(build_jwt(&test), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoiQUJDIn0.i1J1hhBpHZT70_gZZipdE_cWsZJxAoK3p03I7FjcfsA");
+	assert_eq!(build_jwt(&test).unwrap(), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoiQUJDIn0.i1J1hhBpHZT70_gZZipdE_cWsZJxAoK3p03I7FjcfsA");
 }
 
 #[wasm_bindgen_test]
 fn object_2() {
 	console_error_panic_hook::set_once();
-	use wasm_bindgen::{JsCast, JsValue};
 	let test = js_sys::Function::new_no_args("return {test: 123}").call0(&JsValue::NULL).unwrap()
 		.dyn_into::<js_sys::Object>()
 		.unwrap();
-	assert_eq!(build_jwt(&test), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoxMjN9.yFfpIQpxtxzaXgLENVd9yDOfq9rA9l17E2aPLqmRmiA");
+	assert_eq!(build_jwt(&test).unwrap(), "eyJhbGciOiJIUzI1NiJ9.eyJ0ZXN0IjoxMjN9.yFfpIQpxtxzaXgLENVd9yDOfq9rA9l17E2aPLqmRmiA");
+}
+
+#[wasm_bindgen_test]
+fn scramble_macro() {
+	console_error_panic_hook::set_once();
+	waiter_client_auth_macros::scramble!("JWT_SECRET");
+	let test = JWT_SECRET.iter()
+    .map(
+			|(m, c)| (m^c) as char
+		).collect::<String>();
+	assert_eq!(test, "Test");
 }
