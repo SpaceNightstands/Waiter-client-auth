273511f7912d4947f9ee70b204935dabe81d8665
Systool <systool@magisystem.xyz>
2020-10-26 18:02:11 +0100

    Now using macro to embedd secret in wasm

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,24 @@ use wasm_bindgen::prelude::*;
 use js_sys::Object;
 use hmac::Hmac;
 use sha2::Sha256 as SHA;
+use std::collections::HashMap;
+use jwt::token::signed::SignWithKey;
+use serde_json::Value as JsonValue;
 
 // Use `wee_alloc` as the global allocator.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[cfg(not(debug_assertions))]
-//Should scramble with const fn
-const JWT_SECRET: &str = dotenv_codegen::dotenv!("JWT_SECRET");
-#[cfg(debug_assertions)]
-const JWT_SECRET: &str = "Test";
+waiter_client_auth_macros::scramble!("JWT_SECRET");
 
 lazy_static::lazy_static! {
 	static ref JWT_KEY: Result<Hmac<SHA>, String> = {
 		use hmac::NewMac;
-		Hmac::<SHA>::new_varkey(JWT_SECRET.as_bytes())
+		let key = JWT_SECRET.iter()
+			.map(
+				|(m, c)| (m^c)
+			).collect::<Vec<u8>>();
+		Hmac::<SHA>::new_varkey(&*key)
 			.map_err(
 				|err| {
 					let error = format!("{:?}", err);
@@ -29,10 +32,6 @@ lazy_static::lazy_static! {
 
 #[wasm_bindgen]
 pub fn build_jwt(val: &Object) -> Result<String, JsValue> {
-	use std::collections::HashMap;
-	use jwt::token::signed::SignWithKey;
-	use serde_json::Value as JsonValue;
-
 	let key = JWT_KEY.as_ref()
 		.map_err(
 			|err| wasm_bindgen::throw_str(err)
